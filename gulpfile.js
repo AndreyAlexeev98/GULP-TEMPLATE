034cdbfb03115be2f5934d16b41c14ba1e6f8cca
Andrey Alexeev <ipalexeev98@mail.ru>
2023-03-02 22:29:26 +0300

    add creal task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 // src('относительный от gulpfile путь')
 // dest - записывает файлы в ('папку')
 // pipe - реализует механизм потоков
-const { src, dest } = require('gulp');
+// чтобы не писать gulp.task, метод task можно экспортировать в переменную
+const { src, dest, task } = require('gulp');
+const rm = require( 'gulp-rm' ); // Плагин удаления файлов
+
+task( 'clean', () => {
+    return src( 'dist/**/*', { read: false }).pipe( rm() )
+  });
 
 function copyFile() {
-    return src('src/styles/main.scss').pipe( dest('dist') ) 
+    return src('src/styles/**/*.scss').pipe( dest('dist') ) 
 }
 
-// Экспортируем 
+// Экспортируем задачу, если мы её описали не методом task а через функцию 
 exports.copy = copyFile; 
