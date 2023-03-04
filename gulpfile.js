6540ab0d73c086a576b1ba87876a0fedad7a07a2
Andrey Alexeev <ipalexeev98@mail.ru>
2023-03-04 18:03:38 +0300

    create dev and prod mode

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@
 // pipe - реализует механизм потоков (streams) в node.js
 // чтобы не писать gulp.task, метод task можно экспортировать в переменную
 
-const { src, dest, task, series, watch, parallel } = require('gulp'); // чтобы не испортировать весь gulp, берем только то что используетс
+const { src, dest, task, series, watch, parallel } = require('gulp'); // чтобы не испортировать весь gulp, берем только то что будем использовать
 const rm = require( 'gulp-rm' ); // Плагин удаления файлов
 const sass = require('gulp-sass')(require('node-sass')); // Компялятор scss
 const cleanCSS = require('gulp-clean-css'); // Минификация css
-const rename = require("gulp-rename"); // Переименование файлов (например для добавления .min постфикса в минифицированный файл)
+const rename = require("gulp-rename"); // Переименование файлов 
 const sourcemaps = require('gulp-sourcemaps'); // формирует карту файла
 const autoprefixer = require('gulp-autoprefixer'); // добавляет префексеры css
 const concat = require('gulp-concat'); // конкатинация (склейка) файлов в один
@@ -15,11 +15,13 @@ const browserSync = require('browser-sync').create(); // Создание сер
 const reload = browserSync.reload; // Для перезагрузки страницы
 const sassGlob = require('gulp-sass-glob'); // Для реализации маски при импорте стилей
 const px2rem = require('gulp-smile-px2rem'); // Для перевода пикселей в rem
-// const gcmq = require('gulp-group-css-media-queries'); // Группировка медиазапросов для оптимизации итогового css файла
+const gcmq = require('gulp-group-css-media-queries'); // Группировка медиазапросов для оптимизации
 const babel = require('gulp-babel'); // Транскомпилятор JS
 const uglify = require('gulp-uglify'); // Минификация JS
 const svgo = require('gulp-svgo'); // Оптимизация xml кода
 const svgSprite = require('gulp-svg-sprite'); // Для склейки svg в спрайт
+const gulpif = require('gulp-if'); // для разделения используемых плагинов в dev и prod режимах
+const env = process.env.NODE_ENV; // для работы с переменными окружения 
 
 // Импорт конфигурации проекта
 const { SRC_PATH, DIST_PATH, STYLE_LIBS, JS_LIBS, SRC_ALL_HTML_PATH, SRC_ALL_SCSS_PATH, SRC_MAIN_SCSS_PATH, SRC_ALL_JS_PATH, SRC_SVG_PATH, SRC_PICTURE_PATH, MEDIA_ICONS_PATH, MEDIA_PICTURE_PATH } = require('./gulp.config');
@@ -68,41 +70,62 @@ task( 'copy:pictures', () => {
 // Работа со стилями
 task( 'styles', () => {
   return src([...STYLE_LIBS, SRC_MAIN_SCSS_PATH])
-    .pipe(concat('main.min.scss'))
-    .pipe(sourcemaps.init())
+    .pipe(concat('main.scss'))
+    .pipe(gulpif(env === 'dev', sourcemaps.init()))
     .pipe(sassGlob())
     .pipe( (sass().on('error', sass.logError)) )
     .pipe(px2rem())
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['> 1%']
-    }))
-    .pipe(cleanCSS())
-    .pipe(sourcemaps.write())
-    // .pipe(gcmq())
+    .pipe(gulpif(env === 'prod', autoprefixer({
+      overrideBrowserslist: ['> 1%'],
+      cascade: false
+    })))
+    .pipe(gulpif(env === 'prod', gcmq()))
+    .pipe(gulpif(env === 'prod', cleanCSS()))
+    .pipe(gulpif(env === 'dev', sourcemaps.write()))
     .pipe(dest(DIST_PATH))
     .pipe(reload({ stream: true }));
 });
 
-// Минификация css
-task('minify:css', () => {
+// Переименование css (для prod)
+task('rename_main_css', () => {
   return src(`${DIST_PATH}/main.css`)
-    .pipe(cleanCSS())
     .pipe(rename(function(path) {
       path.extname = ".min.css";
     }))
     .pipe(dest(DIST_PATH));
 });
 
+// Переименование css (для prod)
+task('rename_main_js', () => {
+  return src(`${DIST_PATH}/main.js`)
+    .pipe(rename(function(path) {
+      path.extname = ".min.js";
+    }))
+    .pipe(dest(DIST_PATH));
+});
+
+// Удалание main.css (для prod)
+task('remove_main_css', () => {
+  return src(`${DIST_PATH}/main.css`)
+    .pipe( rm() )
+});
+
+// Удалание main.js (для prod)
+task('remove_main_js', () => {
+  return src(`${DIST_PATH}/main.js`)
+    .pipe( rm() )
+});
+
 // Работа с JsvaScript
 task('scripts', () => {
   return src([...JS_LIBS, SRC_ALL_JS_PATH])
-    .pipe(sourcemaps.init())
-    .pipe(concat('main.min.js', {newLine: ';'})) // new line - установка между модулями знака - ';', для избежания ошибок при интерпритации
-    .pipe(babel({
+    .pipe(gulpif(env === 'dev', sourcemaps.init()))
+    .pipe(concat('main.js', {newLine: ';'})) // new line - установка между модулями знака - ';', для избежания ошибок при интерпритации
+    .pipe(gulpif(env === 'prod', babel({
       presets: ['@babel/env']
-    }))
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
+    })))
+    .pipe(gulpif(env === 'prod', uglify()))
+    .pipe(gulpif(env === 'dev', sourcemaps.write()))
     .pipe(dest(DIST_PATH))
     .pipe(reload({ stream: true }));
  });
@@ -122,17 +145,29 @@ task('server', () => {
 });
 
 // Запускаем определенные такски при изменении определенных исходных файлов:
-watch(SRC_ALL_HTML_PATH, series('copy:html'));
-watch(SRC_SVG_PATH, series('icons'));
-watch(SRC_PICTURE_PATH, series('copy:pictures'));
-watch(SRC_ALL_SCSS_PATH, series('styles'));
-watch(SRC_ALL_JS_PATH, series('scripts'));
+task('watch', () => {
+  watch(SRC_ALL_HTML_PATH, series('copy:html'));
+  watch(SRC_SVG_PATH, series('icons'));
+  watch(SRC_PICTURE_PATH, series('copy:pictures'));
+  watch(SRC_ALL_SCSS_PATH, series('styles'));
+  watch(SRC_ALL_JS_PATH, series('scripts'));
+});
 
-// Сначало идет очистка выходных файлов, затем паралельно (для оптимизации) выполняются таски которые могут не ждать завершения друг друга, затем запускается сервер
-task('default', 
+// Для деплоя на сервер
+task('prod',
+  series(
+    'clean', 
+    parallel('copy:html', 'copy:pictures', 'icons', 'styles', 'scripts'),
+    parallel('rename_main_css', 'rename_main_js'),
+    parallel('remove_main_css', 'remove_main_js')
+ )
+);
+
+// Сначало идет очистка выходных файлов, затем паралельно (для оптимизации) выполняются таски которые могут не ждать завершения друг друга, затем запускается сервер и вотчеры
+task('dev', 
   series(
     'clean', 
     parallel('copy:html', 'copy:pictures', 'icons', 'styles', 'scripts', ),  
-    'server'
+    parallel('watch', 'server')
   )
 );
